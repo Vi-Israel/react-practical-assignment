c9660ead2844cf0483410e72c26e903798b8a5c6
Vi <slaprok777@gmail.com>
2023-12-21 13:57:53 +0200

    add getPostsByPage in postsAction.js

--- client/src/redux/actions/postsAction.js
+++ client/src/redux/actions/postsAction.js
@@ -1,3 +1,4 @@
+import {base_url} from "../../utils/constants";
 
 export const CHANGE_POSTS = 'CHANGE_POSTS';
 
@@ -6,3 +7,11 @@ export const changePosts = posts =>
         type: CHANGE_POSTS,
         payload: posts
     });
+
+export const getPostsByPage= number=>{
+    return dispatch=>{
+    fetch(base_url+`post/page/${number}`)
+        .then(response=>response.json())
+        .then(data=> dispatch(changePosts(data.result)))
+        .catch(()=>console.log("Error in posts"))
+}}
