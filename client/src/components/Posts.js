1ab4f69fdf1fca8c26c8886f199e2dcad6d694c6
Vi <slaprok777@gmail.com>
2023-12-21 15:32:32 +0200

    fix package.json libraries

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {getPostsByKeyword, getPostsByPage} from "../redux/actions/postsAction";
 import Post from "./Post";
